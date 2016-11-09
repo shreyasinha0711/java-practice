c167f69af34bf6e2f06bba2a193d96df54f5557d
shreyasinha0711 <shreyasinha0711@gmail.com>
2016-11-09 14:50:03 +0530

    welcome commit

--- src/prac/Hello.java
+++ src/prac/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("HELLO WORLD!!!");
-		System.out.println("WELCOME!!!");
+		System.out.println("WELCOOME!!!");
 
 	}
 
